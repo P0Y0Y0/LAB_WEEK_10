56df113a32c8534b28c2aae216706d56f775ea67
P0Y0Y0 <erufumang@gmail.com>
2025-11-21 17:25:07 +0700

    Commit No. 1: Implement ViewModel to Handle Rotation (updated)

--- app/src/main/java/com/example/lab_week_10/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_10/MainActivity.kt
@@ -7,28 +7,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.example.lab_week_10.viewmodels.TotalViewModel
 
 class MainActivity : AppCompatActivity() {
-    private var total: Int = 0
+
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        total = 0
-        updateText(total)
-        findViewById<Button>(R.id.button_increment).setOnClickListener {
-            incrementTotal()
-        }
-    }
 
-    private fun incrementTotal() {
-        total++
-        updateText(total)
+        prepareViewModel()
     }
 
     private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
             getString(R.string.text_total, total)
+    }
+
+    private fun prepareViewModel(){
+        viewModel.total.observe(this) { total ->
+            updateText(total)
+        }
+
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+        }
     }
 }
